46882c9bcf47727eabaf0310942a14d4802bb598
oktarian siswanto <rian15194@gmail.com>
2022-10-09 21:11:49 +0700

    Add project files.

---
+++ BelajarReview.Contracts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BelajarReview.Contracts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
